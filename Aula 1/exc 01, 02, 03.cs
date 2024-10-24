054086ed57bb50929e883b196a808c3a57f7a754
isabely1425 <127849825+isabely1425@users.noreply.github.com>
2024-10-24 08:59:43 -0300

    Add files via upload

---
+++ Aula 1/exc 01, 02, 03.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+  public static void Main (string[] args) {
+    //Exemplo 1-----------------------------------------------------------------
+    Console.WriteLine ("Digite alguma coisa no teclado: ");
+
+
+    //Capiturando a entrada do usuário------------------------------------------
+    string entrada = Console.ReadLine();
+
+  //exibindo o que o usuario digitou--------------------------------------------
+    Console.WriteLine("Você digitou: " + entrada);
+    }
+  }
